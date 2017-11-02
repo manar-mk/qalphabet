b695ae5930d38ac8cfc2628f0149c26ad15fe91c
Manar Kurmanov <kurmanov.manar@gmail.com>
2017-11-03 02:47:12 +0600

    vue added

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app container-fluid" id="app">
+    <div class="jumbotron text-center">
+        <h1>QAlphabet</h1>
+        <p class="lead">
+            <span class="text-warning">Қазақша мәтінді латын нұсқасына аудару</span>
+            <br>
+            <span class="text-success">Qazaqs'a ma'tіndі latyn nusqasyna ay'dary'</span>
+        </p>
+        <a target="_blank" class="btn btn-primary btn-lg" href="mailto:contact@qalphabet.kz">
+            <i class="glyphicon glyphicon-envelope"></i>
+            contact@qalphabet.kz
+        </a>
+    </div>
+    <QConvert/>
+  </div>
+</template>
+
+<script>
+import QConvert from './components/QConvert';
+
+export default {
+  name: 'app',
+  components: {
+    QConvert
+  }
+};
+</script>
+
+<style scoped>
+.app {
+  padding-top: 10px;
+  padding-bottom: 30px;
+}
+.jumbotron {
+  padding: 30px;
+}
+.jumbotron p {
+  font-weight: 400;
+}
+.jumbotron h1 {
+  font-size: 45px;
+}
+.btn {
+  font-size: 14px;
+}
+.btn .glyphicon {
+  font-size: 12px;
+  margin-right: 3px;
+}
+</style>
---
+++ src/components/QConvert.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="main">
+    <div class="field">
+        <label for="cyr" class="control-label text-warning">Кирил жазуы</label>
+        <textarea class="cyr form-control" @keyup="submit" id="cyr" v-model="cyr"></textarea>
+    </div>
+    <div class="field">
+        <label for="lat" class="control-label text-success">Латын жазуы</label>
+        <textarea class="lat form-control" id="lat" v-model="lat"></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+import qazLatyn from 'qazlatyn-db';
+
+export default {
+  name: 'QConvert',
+  data() {
+    return {
+      cyr:
+        'Бұл жасқа келгенше жақсы өткіздік пе, жаман өткіздік пе, әйтеуір бірталай өмірімізді өткіздік: алыстық, жұлыстық, айтыстық, тартыстық - әурешілікті көре-көре келдік. Енді жер ортасы жасқа келдік: қажыдық, жалықтық; қылып жүрген ісіміздің баянсызын, байлаусызын көрдік, бәрі қоршылық екенін білдік. Ал, енді қалған өмірімізді қайтіп, не қылып өткіземіз? Соны таба алмай өзім де қайранмын.',
+      lat: ''
+    };
+  },
+  mounted: function() {
+    this.submit();
+  },
+  methods: {
+    submit: function() {
+      this.lat = this.cyr
+        .split('')
+        .map(function(el) {
+          if (qazLatyn.hasOwnProperty(el)) return qazLatyn[el];
+
+          return el;
+        })
+        .join('');
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+textarea.form-control {
+  width: 100%;
+  display: block;
+  resize: vertical;
+  min-height: 100px;
+  color: #ffffff;
+}
+.field {
+  margin-top: 20px;
+}
+.cyr {
+  background: #fd9d4f;
+}
+.lat {
+  background: #4dc54f;
+}
+</style>
